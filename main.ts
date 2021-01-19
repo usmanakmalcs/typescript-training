5e1d54cc4d32456276153cc80c2aa235884638b6
Usman Akmal <usman.akmal@synavos.com>
2021-01-19 23:09:05 +0500

    Type annotation in TypeScript

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
-function log(message){
-    console.log(message);
-}
+let a:number;                           // Store only number
+let b:string;                           // Store only string
+let c:boolean;                          // Store only boolean
+let d:any;                              // Store any type of value
 
-var logMessage = "My First Transcript Code";
-log(logMessage);
+let e:number[] = [1,2,3];               // Default initialization in array of type number
+let f:string[] = ["1","2","3"];         // Default initialization in array of type string
+let g:boolean[] = [true,false,true];    // Default initialization in array of type boolean
+let h:any[] = [1,"2",true];             // Default initialization in array of type any
